5e7276eac712981918baccb3ee2f173f03820138
Sinitskikh <158315462+Sinitskikh@users.noreply.github.com>
2024-01-31 23:02:37 +0500

    Add files via upload

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,16 @@
+
+
+def items_search(item_list, find_item):
+    for number, item in enumerate(items_list):
+        if item == find_item:
+            return number
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = items_search(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
